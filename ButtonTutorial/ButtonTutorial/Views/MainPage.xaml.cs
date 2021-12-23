02a3b6ba61635c7eb60586ae000c08253c4f7a13
Монгол <dtrofimenko.523@gmail.com>
2021-12-24 02:04:05 +0300

    iff

---
+++ ButtonTutorial/ButtonTutorial/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ButtonTutorial.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+    }
+}
